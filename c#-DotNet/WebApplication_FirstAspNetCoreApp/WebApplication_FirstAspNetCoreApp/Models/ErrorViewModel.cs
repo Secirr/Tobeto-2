9ed4affffa75612f68a9a28f309d265e8394eb2c
Kadir Özdemir <kadirozdemirr00@gmail.com>
2023-11-07 23:22:11 +0300

    starting dotnet

---
+++ c#-DotNet/WebApplication_FirstAspNetCoreApp/WebApplication_FirstAspNetCoreApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication_FirstAspNetCoreApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
